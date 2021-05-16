c61a6667102206d3efa93eb210d4390e072aa8a3
unknown <sergio-caballero_student2021@wilder.school>
2021-05-16 22:54:52 +0200

    added useRef

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -7,6 +7,7 @@ export const useFetch = ( url ) => {
     const [state, setState] = useState({ data: null, loading: true, error: null });
     
     useEffect( () => {
+        //it triggers when the component is dismounted
         return () => {
             isMounted.current = false;
         }
@@ -14,14 +15,14 @@ export const useFetch = ( url ) => {
 
     // Once we receive an url we do fetch
     useEffect( () => {
-        
+
         //display loading everytime we change the quote
         setState({ data: null, loading: true, error: null });
 
         fetch( url )
             .then( resp => resp.json() )
             .then( data => {
-
+                //We do not set the state until the component is mounted
                 if ( isMounted.current ) {
                     setState({
                         loading: false,
